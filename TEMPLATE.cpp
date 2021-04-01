6b2923c1ea5d92009c72f7b3d842bb8006901e40
Neville Joseph Sabu <69287021+NevilleJS@users.noreply.github.com>
2021-04-01 19:16:15 +0530

    Update TEMPLATE.cpp

--- TEMPLATE.cpp
+++ TEMPLATE.cpp
@@ -3,8 +3,7 @@ using namespace std;
 typedef long long ll;
 #define INF (int)1e9
 #define PI 3.1415926535897932384626433832795
-
-const ll mod = 1e9 + 7;
+#define MOD 1e9+7
 
 void sed(){
     ios_base::sync_with_stdio(0);
