3950f545f978f304c3fa24502207419abbdc1d2a
NevilleJS <nevillejs24@gmail.com>
2021-04-01 19:06:47 +0530

    SDE

---
+++ TEMPLATE.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define INF (int)1e9
+#define PI 3.1415926535897932384626433832795
+
+const ll mod = 1e9 + 7;
+
+void sed(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    // #ifndef ONLINE_JUDGE
+    // freopen("i.txt", "r", stdin);
+    // freopen("o.txt", "w", stdout);
+    // #endif
+}
+ 
+
+int main()
+{
+    sed();
+
+}
